61b5a0d1b1990491dba5461138922a9dc9f42ad1
kirill675-bit <k73396134@gmail.com>
2026-09-15 20:59:12 +0500

    1.1

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone myPhone1 = new Samsung();
+            myPhone1.turn();
+            Smartphone myPhone2 = new Redmi();
+            myPhone2.turn();
+        }
+
+    }
+    internal class Smartphone
+    {
+
+        private String name;
+        private int Age;
+        public String owner;
+
+        public virtual void turn()
+        {
+            Console.WriteLine("Smartphone turn");
+        }
+    }
+
+
+
+    internal class Samsung : Smartphone
+    {
+        public override void turn()
+        {
+            Console.WriteLine("Samsung turn");
+        }
+    }
+
+
+
+    internal class Redmi : Smartphone
+
+    {
+        public override void turn()
+        {
+            Console.WriteLine("redmi turn");
+        }
+
+    }
+
+
+
+    internal abstract class program1
+    {
+        public abstract void action();
+    }
+}
+
